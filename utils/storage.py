9473fb0695fa68b429a183e236bf5bdc5628e21c
dawidw-km <dawidwieclaw@hotmail.com>
2025-07-27 19:41:14 +0200

    storage

--- utils/storage.py
+++ utils/storage.py
@@ -1,24 +1,7 @@
-import json
-from core.models.tracker import Tracker
-
-filename = "C:/Users/Dawid/Documents/VSC/DailyLog/data/tracker.json"
+from sqlalchemy import create_engine, Integer, String, Float, Column
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 
-def save_tracker_to_file(trackers):
-    with open(filename, "w") as f:
-        json.dump([tracker.to_dict() for tracker in trackers], f, indent=4)
+engine = create_engine('sqlite:///mydatabase_orm.db', echo=True)
 
-def load_tracker_from_file():
-    try:
-        with open(filename, "r") as f:
-            data = json.load(f)
-            if not data:
-                Tracker._id_counter = 1
-                return []
-            return [Tracker.from_dict(tracker_dict) for tracker_dict in data]
-    except FileNotFoundError:
-        Tracker._id_counter = 1
-        return []
-    except json.JSONDecodeError:
-        ("File empty or incorrect format")
-        return []
+Base = declarative_base()
